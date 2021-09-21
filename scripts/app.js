09aebf7bc219e79b8df81ffeaea73b357153a86d
Christian Johannesson <christian.johannesson@drakryggen.com>
2021-09-21 10:47:09 +0200

    Mobile menu and responsivity done!

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 let menuOpen = false;
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".mobile-menu");
+const links = document.getElementsByClassName("mobile-link");
+const documentBody = document.body;
 
 function sendFormInfo() {
   alert("Your message has been sent!");
@@ -8,11 +10,15 @@ function sendFormInfo() {
 
 function toggleMenu() {
   if (!menuOpen) {
+    menuBtn.classList.add("open");
     menuOpen = true;
     menu.style.right = "0%";
+    documentBody.style.overflow = "hidden";
   } else {
+    menuBtn.classList.remove("open");
     menuOpen = false;
     menu.style.right = "-100%";
+    documentBody.style.overflow = "auto";
   }
 }
 
@@ -20,4 +26,9 @@ window.addEventListener("load", function () {
   menuBtn.addEventListener("click", function () {
     toggleMenu();
   });
+  for (let i = 0; links.length; i++) {
+    links[i].addEventListener("click", function () {
+      toggleMenu();
+    });
+  }
 });
