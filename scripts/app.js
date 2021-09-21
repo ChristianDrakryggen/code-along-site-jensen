f30689fe0e4a0934b9b7d1b73d973653d7a1f0b9
Christian Johannesson <christian.johannesson@drakryggen.com>
2021-09-21 09:48:29 +0200

    added mobile-menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,23 @@
+let menuOpen = false;
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".mobile-menu");
+
 function sendFormInfo() {
   alert("Your message has been sent!");
 }
+
+function toggleMenu() {
+  if (!menuOpen) {
+    menuOpen = true;
+    menu.style.right = "0%";
+  } else {
+    menuOpen = false;
+    menu.style.right = "-100%";
+  }
+}
+
+window.addEventListener("load", function () {
+  menuBtn.addEventListener("click", function () {
+    toggleMenu();
+  });
+});
